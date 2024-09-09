b9864d2b1587f149b20ca177e0466205639bebba
DD <DD>
2024-09-09 21:30:40 +0200

    1st for round

---
+++ my_round/my_round.c
@@ -0,0 +1,21 @@
+int my_round(float n)
+{
+    int cons = n;
+    float dv = n - cons;
+    if (n>=0)
+    {
+        if ( dv >= 0.5 )
+        {
+            return cons + 1;
+        }
+        return cons;
+    }
+    else
+    {
+        if ( dv >= 0.5 )
+        {
+            return cons - 1;
+        }
+        return cons;
+    }
+}
