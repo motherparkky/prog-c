8241e83723f5a0ea210a93f9dda512bc72313697
DD <DD>
2024-09-09 22:10:13 +0200

    first version

---
+++ digit/digit.c
@@ -0,0 +1,13 @@
+unsigned int digit(int n, int k)
+{
+    if (n <= 0 || k <= 0)
+    {
+        return 0;
+    }
+    int div = 10;
+    for (int i = 0 ; i < k - 1 ; i++ )
+    {
+        div *= div;
+    }
+    return (n % div) / (div / 10);
+}
