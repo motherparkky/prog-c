3b3980325a4dfc6d10035b493850f364ea69ed3d
DD <DD>
2024-09-11 18:58:07 +0200

    2nd version

--- insertion_sort/insertion_sort.c
+++ insertion_sort/insertion_sort.c
@@ -1,18 +1,19 @@
+#include <stddef.h>
 void insertion_sort(int array[], size_t size)
 {
     int sorted_arr[size];
     int index = 0;
-    for ( int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
         index = 0;
         while (sorted_arr[index] < array[i])
         {
             index++;
         }
-        for ( int j = index; j < i; j++)
+        for (int j = index; j < i; j++)
         {
-            temp = sorted_arr[j+1];
-            sorted_arr[j+1] = sorted_arr[j];
+            temp = sorted_arr[j + 1];
+            sorted_arr[j + 1] = sorted_arr[j];
         }
         sorted_arr[index] = array[i];
     }
