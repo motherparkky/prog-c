2923d728043d9d60f2ea7710ce68f41d34bc9781
DD <DD>
2024-09-11 16:20:52 +0200

    first version

---
+++ fibo/fibo.c
@@ -0,0 +1,11 @@
+unsigned long fibonacci(unsigned long n)
+{
+    if ( n == 0 || n == 1)
+    {
+        return n;
+    }
+    else
+    {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
