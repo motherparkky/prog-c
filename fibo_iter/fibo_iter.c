c13e27a601bcf805045d2579ec8cdcefb0fe27e1
DD <DD>
2024-09-11 17:15:59 +0200

    2nd version

--- fibo_iter/fibo_iter.c
+++ fibo_iter/fibo_iter.c
@@ -6,10 +6,10 @@ unsigned long fibo_iter(unsigned long n)
     }
     else
     {
-        unsigned long u0 == 0;
-        unsigned long u1 == 1;
+        unsigned long u0 = 0;
+        unsigned long u1 = 1;
         unsigned long ans = 0;
-        for (int i = 0; i < n - 1; i++)
+        for (unsigned long int i = 0; i < n - 1; i++)
         {
             ans = u0 + u1;
             u0 = u1;
