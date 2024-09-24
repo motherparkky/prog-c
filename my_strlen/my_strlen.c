d449bdd357b6bb3480b19d2ca536337c3aee1347
DD <DD>
2024-09-24 23:51:09 +0200

    first version

---
+++ my_strlen/my_strlen.c
@@ -0,0 +1,13 @@
+#include "my_strlen.h"
+
+#include <stddef.h>
+
+size_t my_strlen(const char *s)
+{
+    size_t len = 0;
+    while(s[len] != '\0')
+    {
+        len++;
+    }
+    return len;
+}
---
+++ my_strlen/my_strlen.h
@@ -0,0 +1,9 @@
+#ifndef MY_STRLEN_H
+#define MY_STRLEN_H
+
+#include <stddef.h>
+
+size_t my_strlen(const char *s);
+
+#endif /* !MY_STRLEN_H */
+
